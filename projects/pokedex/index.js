97f84067a3cc2663a93cf51e9047fd825d2276b0
Macharete_r <rafaelmacharete00@gmail.com>
2025-02-01 09:12:25 -0300

    done: Pokedéx

--- projects/pokedex/index.js
+++ projects/pokedex/index.js
@@ -6,15 +6,63 @@ let currentIndex = 1
 const nextButton = document.querySelector('#next')
 const previousButton = document.querySelector('#previous')
 
+const input = document.querySelector('#input')
+
+const typeColors = {
+    normal: "#A8A77A",
+    fire: "#EE8130",
+    water: "#6390F0",
+    electric: "#F7D02C",
+    grass: "#7AC74C",
+    ice: "#96D9D6",
+    fighting: "#C22E28",
+    poison: "#A33EA1",
+    ground: "#E2BF65",
+    flying: "#A98FF3",
+    psychic: "#F95587",
+    bug: "#A6B91A",
+    rock: "#B6A136",
+    ghost: "#735797",
+    dragon: "#6F35FC",
+    dark: "#705746",
+    steel: "#B7B7CE",
+    fairy: "#D685AD"
+};
+
 async function fetchPokemonData(id) {
-    let requisition = await (fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`));
+    let requisition = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
     let json = await requisition.json();
 
-    h1_name = document.querySelector('#name');
+    let h1_name = document.querySelector('#name');
     h1_name.textContent = json.name;
 
+    let h1_id = document.querySelector('h1#id');
+    h1_id.textContent = '#' + json.id;
+
     let pokemonImage = document.querySelector('#pokemon_image');
-    pokemonImage.src = json.sprites['front_default'];
+    pokemonImage.src = json.sprites.front_default;
+
+    let typesContainer = document.querySelector('.types');
+    typesContainer.innerHTML = '';
+
+    let primaryType = json.types[0].type.name;
+
+
+    document.body.style.backgroundColor = typeColors[primaryType] || "#FFF";
+
+    json.types.forEach((typeInfo) => {
+        let typeElement = document.createElement("h1");
+        typeElement.textContent = typeInfo.type.name;
+
+        typeElement.style.backgroundColor = typeColors[typeInfo.type.name];
+        typeElement.style.color = "#FFF";
+        typeElement.style.padding = "5px 10px";
+        typeElement.style.borderRadius = "10px";
+        typeElement.style.display = "inline-block";
+        typeElement.style.margin = "5px";
+
+        typesContainer.appendChild(typeElement);
+    });
 }
 
 nextButton.addEventListener('click', () => {
@@ -30,5 +78,15 @@ previousButton.addEventListener('click', () => {
         fetchPokemonData(currentIndex);
     }
 });
+
+input.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter'){
+        if (input.value <= 0){
+            alert('You entered a value out of range')
+        }
+        currentIndex = input.value
+        fetchPokemonData(input.value)
+    }
+})
 
 fetchPokemonData(currentIndex);
