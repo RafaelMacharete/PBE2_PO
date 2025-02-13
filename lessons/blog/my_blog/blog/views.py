8939116e8d61963a2674a28aa8e0786d6f8c2d1c
Rafael da Silva Macharete <52752592809@>
2025-02-13 16:37:10 -0300

    django exercise

---
+++ lessons/blog/my_blog/blog/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Post
+
+# Create your views here.
+def list_post(request):
+    posts = Post.objects.all().order_by('-creation_date')
+    return render(request, 'blog/list_post.html', {'posts': posts})
