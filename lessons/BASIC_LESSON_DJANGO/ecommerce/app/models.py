4fd96bf61f3d025fe618abd0627d7f849cc8f6cb
RafaelMacharete <rafaelmacharete00@gmail.com>
2025-02-21 13:48:44 -0300

    product page

--- lessons/BASIC_LESSON_DJANGO/ecommerce/app/models.py
+++ lessons/BASIC_LESSON_DJANGO/ecommerce/app/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class Product(models.Model):
     product_name = models.CharField(max_length=50)
     product_image = models.ImageField(upload_to='images/', null=True)
-    product_price = models.CharField(max_length=30) 
+    product_price = models.CharField(max_length=30)
+    product_detail = models.TextField(default='Product')
 
     def __str__(self):
         return self.product_name    
--- lessons/BASIC_LESSON_DJANGO/ecommerce/app/views.py
+++ lessons/BASIC_LESSON_DJANGO/ecommerce/app/views.py
@@ -5,3 +5,6 @@ def index(req):
     products = Product.objects.all().order_by('-product_price')
     return render(req, 'app/index.html', {'products' : products})
     
+def product(req, id):
+    product = Product.objects.get(id=id)
+    return render(req, 'app/product.html', {'product': product})
