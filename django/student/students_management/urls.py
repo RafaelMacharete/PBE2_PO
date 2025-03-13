af35f51a718d0cd02ce5e92f718762b7d050a6f0
RafaelMacharete <rafaelmacharete00@gmail.com>
2025-03-13 16:08:18 -0300

    api documentation

--- django/student/students_management/urls.py
+++ django/student/students_management/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
  
 urlpatterns = [
-    path('students/', views.index),
-    path('students/create/', views.create_stundent)
+    path('students/', views.get_all_students),
+    path('student/<int:pk>/', views.get_student_by_id),
+    path('student/create/', views.create_student),
+    path('student/alter/<int:pk>', views.update_student_info),
+    path('student/delete/<int:pk>', views.delete_student),
+    path('do/something/<str:text>', views.do_something),
 ]
--- django/student/students_management/views.py
+++ django/student/students_management/views.py
@@ -5,14 +5,29 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 
+#Get all students 
 @api_view(['GET'])
-def index(req):
+def get_all_students(req):
     all_students = Student.objects.all()
     serializer = StudentSerializer(all_students, many=True)
     return Response(serializer.data)
 
+# Get student object by ID
+@api_view(['GET'])
+def get_student_by_id(request, pk):
+    try:
+        student_by_pk = Student.objects.get(pk=pk)
+    except Student.DoesNotExist:
+        return Response(
+            f"Error: {status.HTTP_404_NOT_FOUND} NOT FOUND", 
+            status=status.HTTP_404_NOT_FOUND
+            )
+
+    serializer = StudentSerializer(student_by_pk)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
 @api_view(['POST'])
-def create_stundent(req):
+def create_student(req):
     if req.method == 'POST':
         serializer = StudentSerializer(data=req.data)
         if serializer.is_valid():
@@ -20,3 +35,34 @@ def create_stundent(req):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+def update_student_info(req, pk):
+    try:
+        student_to_update = Student.objects.get(pk=pk)
+    except Student.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    serializer = StudentSerializer(student_to_update, data=req.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+def delete_student(req,pk):
+    try:
+        student_to_delete = Student.objects.get(pk=pk)
+    except Student.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    student_to_delete.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET'])
+def do_something(req, text):
+    import pyfiglet
+    field = pyfiglet.figlet_format(text)
+
+    return Response(field, status=status.HTTP_200_OK)
